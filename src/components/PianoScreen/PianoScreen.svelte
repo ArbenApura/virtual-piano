d0d94431b2c7e285e1e74cd03f9de862f96b906c
Arben Apura <arbenapura.official@gmail.com>
2023-01-13 17:07:26 +0800

    Updated PianoScreen

--- src/components/PianoScreen/PianoScreen.svelte
+++ src/components/PianoScreen/PianoScreen.svelte
@@ -6,14 +6,39 @@
 </script>
 
 <div class="screen">
-	{#each pianoKeys as key}
-		<PianoTiles type={key.type} note={key.note} />
-	{/each}
+	<div class="brand">
+		<img src="piano.svg" alt="Logo" />
+		<div class="text">
+			<p>Virtual Piano</p>
+		</div>
+	</div>
+	<div class="tiles">
+		{#each pianoKeys as key}
+			<PianoTiles type={key.type} note={key.note} />
+		{/each}
+	</div>
 </div>
 
 <style lang="scss">
 	@import '$styles';
 	.screen {
-		@apply w-full h-full flex relative;
+		@apply w-full h-full;
+		.brand {
+			@apply absolute z-20 p-2;
+			@include flex-center;
+			img {
+				@apply w-[40px] mr-2;
+			}
+			.text {
+				@apply flex-col;
+				@include flex-center;
+				p {
+					@apply text-slate-50 text-base;
+				}
+			}
+		}
+		.tiles {
+			@apply w-full h-full flex relative;
+		}
 	}
 </style>
